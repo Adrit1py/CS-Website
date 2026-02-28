ac0662adafd34afd13eeb7c85816ea3f45e126be
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-02-28 23:43:34 +0530

    optimize

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -28,19 +28,38 @@ export default function TeamPage() {
     
       <LightFXBackground />
      <div className="absolute inset-0 z-10 pointer-events-none">
-        <LightRays />
+      
+  <LightRays
+     raysOrigin="top-center"
+     raysColor="#ffb43f"
+     raysSpeed={1.5}
+     lightSpread={1}
+     rayLength={3}
+      followMouse={true}
+      mouseInfluence={0.3}
+      noiseAmount={0.1}
+      distortion={0}
+      className="custom-rays"
+      pulsating={false}
+      fadeDistance={1.7}
+      saturation={1}
+    />
+
       </div>
       {/* Spacer for navbar separation */}
-      <div className="relative z-10 h-96"></div>
+      <div className="relative z-10 h-30"></div>
       
       <div className="relative z-10 pb-20">
+        <div className="text-3xl md:text-6xl font-bold text-center text- h-50" style={{ color: '#FFA300' }}>
+          <p>Meet Our Team</p>
+          </div>
 
           {/* Executive Committee Section */}
           <div className="w-full flex flex-col items-center">
             <h2 className="text-4xl md:text-5xl font-bold text-center mb-12" style={{ color: '#FFA300' }}>
               Executive Committee
             </h2>
-            <div className="border-b border-gray-600 w-3/4"></div>
+            <div className="border-b border-white-600 w-3/4"></div>
             
             {/* Spacer between line and cards */}
             <div className="h-20"></div>
